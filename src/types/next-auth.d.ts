d07df937360e0dfbd843e0d5eab0f416b87b9a18
priyanshub18 <priyanshubhardwaj658@gmail.com>
2025-02-24 16:55:16 +0530

    added-all-dashboard-user-all-pages

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,7 +1,7 @@
 import "next-auth";
 import { DefaultSession } from "next-auth";
 
-declare module 'next-auth' {
+declare module "next-auth" {
   interface User {
     _id?: string;
     isVerified?: boolean;
@@ -14,7 +14,7 @@ declare module 'next-auth' {
       isVerified?: boolean;
       isAcceptingMessage?: boolean;
       username?: string;
-    } & DefaultSession['user'];
+    } & DefaultSession["user"];
   }
 }
 
