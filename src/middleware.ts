06892535508024cc038bed5b1fcc5f776538dd5a
priyanshub18 <priyanshubhardwaj658@gmail.com>
2025-02-26 15:34:08 +0530

    fix-by-adding-some-artificial-delay

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,19 +7,22 @@ export const config = {
 };
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
-  console.log(token);
-  const url = request.nextUrl;
+  setTimeout(async () => {
+    const token = await getToken({ req: request });
+    console.log("adding some artifical delay to mitigate the population of the database");
+    console.log(token);
+    const url = request.nextUrl;
 
-  // Redirect to dashboard if the user is already authenticated
-  // and trying to access sign-in, sign-up, or home page
-  if (token && (url.pathname.startsWith("/sign-in") || url.pathname.startsWith("/sign-up") || url.pathname.startsWith("/verify"))) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+    // Redirect to dashboard if the user is already authenticated
+    // and trying to access sign-in, sign-up, or home page
+    if (token && (url.pathname.startsWith("/sign-in") || url.pathname.startsWith("/sign-up") || url.pathname.startsWith("/verify"))) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
 
-  if (!token && url.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
+    if (!token && url.pathname.startsWith("/dashboard")) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
 
-  return NextResponse.next();
+    return NextResponse.next();
+  }, 2000);
 }
